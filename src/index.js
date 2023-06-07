1c849515e550ed91c2b6c8239746f2cff15a7e02
Javier Grau <javiergrau.freelance@gmail.com>
2023-06-06 21:22:47 -0400

    Linters installation

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import _ from "lodash";
+import "./styles.css";
+
+function component() {
+  const element = document.createElement("div");
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  return element;
+}
+
+document.body.appendChild(component());
