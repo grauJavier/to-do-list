3d40a142d57807168af5c5a4dff7fc2f5139e980
Javier Grau <javiergrau.freelance@gmail.com>
2023-06-08 15:19:47 -0400

    Minor changes on 'listTitle.Value' when webpages load

--- src/modules/loader.js
+++ src/modules/loader.js
@@ -4,7 +4,7 @@ import printHTML from './printHTML.js';
 const loader = () => {
   const listTitle = document.querySelector('#title-box__title');
   const taskList = JSON.parse(localStorage.getItem('taskListData')) || [];
-  listTitle.value = JSON.parse(localStorage.getItem('title')) || 'To-do List Title';
+  listTitle.value = JSON.parse(localStorage.getItem('title')) || 'A Nice To-do List Title!';
 
   if (taskList.length !== 0) {
     for (let i = 0; i < taskList.length; i += 1) {
